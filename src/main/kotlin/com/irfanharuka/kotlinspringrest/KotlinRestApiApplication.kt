a3e454b9905ea16a66eeed89ba4ec6688d30bebf
Irfan Irawan Sukirman <irfan.sukirman@harukaedu.com>
2020-11-08 10:47:00 +0700

    fix: init

---
+++ src/main/kotlin/com/irfanharuka/kotlinspringrest/KotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.irfanharuka.kotlinspringrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestApiApplication>(*args)
+}
